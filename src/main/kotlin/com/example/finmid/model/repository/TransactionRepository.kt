ab42c6aca992ca489f65a2a444ec48e508648a4d
nuwak <mr.igorr@gmail.com>
2022-04-25 08:49:29 +0400

    removed commented code

--- src/main/kotlin/com/example/finmid/model/repository/TransactionRepository.kt
+++ src/main/kotlin/com/example/finmid/model/repository/TransactionRepository.kt
@@ -1,14 +1,6 @@
 package com.example.finmid.model.repository
 
 import com.example.finmid.model.entity.Transaction
-import org.springframework.data.jpa.repository.Modifying
-import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
-import java.math.BigDecimal
 
-interface TransactionRepository : CrudRepository<Transaction, Long> {
-
-//    @Modifying(clearAutomatically = true)
-//    @Query("update Transaction set balance")
-//    fun transfer(fromBalance: BigDecimal, toBalance: BigDecimal, fromId: Long, toId: Long)
-}
+interface TransactionRepository : CrudRepository<Transaction, Long>
