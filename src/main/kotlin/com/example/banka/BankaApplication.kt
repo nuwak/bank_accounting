01b2008f7c3c184d95c172ce4fbd3144ddd24830
nuwak <mr.igorr@gmail.com>
2022-05-04 13:33:27 +0400

    rename classes

--- src/main/kotlin/com/example/banka/BankaApplication.kt
+++ src/main/kotlin/com/example/banka/BankaApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class bankaApplication
+class BankaApplication
 
 fun main(args: Array<String>) {
-    runApplication<bankaApplication>(*args)
+    runApplication<BankaApplication>(*args)
 }
--- src/test/kotlin/com/example/banka/BankaApplicationTests.kt
+++ src/test/kotlin/com/example/banka/BankaApplicationTests.kt
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class bankaApplicationTests {
+class BankaApplicationTests {
 
     @Test
     fun contextLoads() {
